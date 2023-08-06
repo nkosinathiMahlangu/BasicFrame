3daa1b4335e8771177b00704fa73d8bb91bbaf09
nkosinathiMahlangu <141540920+nkosinathiMahlangu@users.noreply.github.com>
2023-08-06 07:19:17 +0200

    Add files via upload

---
+++ BasicFrame/src/za/ac/tut/myfirstname/MyFirstName.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.myfirstname;
+
+import java.awt.Color;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author MR
+ */
+public class MyFirstName extends JFrame{
+ 
+public MyFirstName(){
+    setTitle("My First GUI");
+    setSize(250,100);
+    setDefaultLookAndFeelDecorated(true);
+    setResizable(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setVisible(true);
+    
+}
+    
+}
